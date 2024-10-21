83facc92186e75fe2b0ff180bde6c556dba6d84b
sya-ri <git@s7a.dev>
2024-10-21 22:03:44 +0900

    chore: Setup prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,23 @@
 import globals from "globals";
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import typescript from "typescript-eslint";
 
-export default [
-    {files: ["**/*.{js,mjs,cjs,ts}"]},
-    {languageOptions: {globals: globals.node}},
+export default typescript.config({
+  files: ["**/*.js", "**/*.ts"],
+  extends: [
+    {
+      files: ["**/*.{js,mjs,cjs,ts}"],
+    },
+    {
+      languageOptions: {
+        globals: globals.node,
+      },
+    },
     js.configs.recommended,
     ...typescript.configs.recommended,
-];
+    eslintPluginPrettierRecommended,
+    eslintConfigPrettier,
+  ],
+});
